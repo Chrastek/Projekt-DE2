4aee4ade6f644e6299f1d8c3445553786621128e
Slavek Rylich <xrylic00@vutbr.cz>
2023-11-19 18:54:11 +0100

    create adc.h and adc.c

---
+++ project/PROJECT-Measuring-electricity/lib/adc/adc.c
@@ -0,0 +1,100 @@
+/***********************************************************************
+ * 
+ * ADC library for AVR-GCC.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2023 Slavek Rylich
+ * Student, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: ACD_select_voltage_ref()
+ * Purpose:  Select voltage reference by REFS1, REFS0.
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+void ACD_select_voltage_ref()
+{
+    ADMUX |= (1<<REFS0);
+}
+
+
+/**********************************************************************
+ * Function: ACD_select_channel()
+ * Purpose:  Select input channel(s).
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+void ACD_select_channel_A0(){ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));}
+void ACD_select_channel_A1(){ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1)); ADMUX |= (1<<MUX0);}
+void ACD_select_channel_A2(){ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX0)); ADMUX |= (1<<MUX1);}
+void ACD_select_channel_A3(){ADMUX &= ~((1<<MUX3) | (1<<MUX2)); ADMUX |= ((1<<MUX0) | (1<<MUX1));}
+void ACD_select_channel_A4(){ADMUX &= ~((1<<MUX3) | (1<<MUX1) | (1<<MUX0)); ADMUX |= (1<<MUX2);}
+void ACD_select_channel_A5(){ADMUX &= ~((1<<MUX3) | (1<<MUX1)); ADMUX |= ((1<<MUX2) | (1<<MUX0));}
+
+
+/**********************************************************************
+ * Function: ACD_enable()
+ * Purpose:  Enable ADC module.
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+void ACD_enable()
+{
+    ADCSRA |= (1<<ADEN);
+}
+
+
+/**********************************************************************
+ * Function: ACD_enable_interrupt()
+ * Purpose:  Enable ADC interrupt.
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+void ACD_enable_interrupt()
+{
+    ADCSRA |= (1<<ADIE);
+}
+
+
+/**********************************************************************
+ * Function: ACD_set_prescaler_X()
+ * Purpose:  Set ADC prescaler for 2,4,8,16,32,64 or 128.
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+//Set prescaler 001 --> 2 
+void ACD_set_prescaler_2(){ADCSRA &= ~((1<<ADPS2) | (1<<ADPS1)); ADCSRA|=(1<<ADPS0)}
+//Set prescaler 010 --> 4 
+void ACD_set_prescaler_4(){ADCSRA &= ~((1<<ADPS2) | (1<<ADPS0)); ADCSRA|=(1<<ADPS1)}
+//Set prescaler 011 --> 8 
+void ACD_set_prescaler_8(){ADCSRA &= ~(1<<ADPS2); ADCSRA|=((1<<ADPS1) | (1<<ADPS0))}
+//Set prescaler 100 --> 16 
+void ACD_set_prescaler_16(){ADCSRA &= ~((1<<ADPS1) | (1<<ADPS0)); ADCSRA|=(1<<ADPS2)}
+//Set prescaler 101 --> 32 
+void ACD_set_prescaler_32(){ADCSRA &= ~(1<<ADPS1); ADCSRA|=((1<<ADPS2) | (1<<ADPS0))}
+//Set prescaler 110 --> 64 
+void ACD_set_prescaler_64(){ADCSRA &= ~(1<<ADPS0); ADCSRA|=((1<<ADPS2) | (1<<ADPS1))}
+//Set prescaler 111 --> 128 
+void ACD_set_prescaler_128(){ADCSRA |= ((1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0));}
+
+
+/**********************************************************************
+ * Function: ACD_start_conv()
+ * Purpose:  Start ADC conversion.
+ * Input(s): none
+ * Returns:  none
+ **********************************************************************/
+void ACD_start_conv()
+{
+    ADCSRA |= (1<<ADSC);
+}
---
+++ project/PROJECT-Measuring-electricity/lib/adc/adc.h
@@ -0,0 +1,162 @@
+
+#ifndef ADC_H
+# define ADC_H
+
+/***********************************************************************
+ * 
+ * ADC library for AVR-GCC.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2023 Slavek Rylich
+ * Student, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/**
+ * @file 
+ * @defgroup rylich_adc ADC Library <adc.h>
+ * @defgroup fryza_gpio GPIO Library <gpio.h>
+ * @code #include <adc.h> @endcode
+ *
+ * @brief ADC library for AVR-GCC.
+ *
+ * The library contains functions for AVR's ADC.
+ *
+ * @note Based on AVR Libc Reference Manual. Tested on ATmega328P 
+ *       (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2.
+ * @author Slavek Rylich, Student, Brno University 
+ *         of Technology, Czechia
+ * @copyright (c) 2023 Slavek Rylich, This work is licensed under 
+ *                the terms of the MIT license
+ * @{
+ */
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+
+
+/* Function prototypes -----------------------------------------------*/
+/**
+ * @brief  Select voltage reference by REFS1, REFS0.
+ * @return none
+ */
+void ACD_select_voltage_ref();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A0();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A1();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A2();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A3();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A4();
+
+
+/**
+ * @brief  Select input channel.
+ * @return none
+ */
+void ACD_select_channel_A5();
+
+
+/**
+ * @brief  Enable ADC module.
+ * @return none
+ */
+void ACD_enable();
+
+
+/**
+ * @brief  Enable ADC interrupt.
+ * @return none
+ */
+void ACD_enable_interrupt();
+
+
+/**
+ * @brief  Set ADC prescaler for 2.
+ * @return none
+ */
+void ACD_set_prescaler_2();
+
+
+/**
+ * @brief  Set ADC prescaler for 4.
+ * @return none
+ */
+void ACD_set_prescaler_4();
+
+
+/**
+ * @brief  Set ADC prescaler for 8.
+ * @return none
+ */
+void ACD_set_prescaler_8();
+
+
+/**
+ * @brief  Set ADC prescaler for 16.
+ * @return none
+ */
+void ACD_set_prescaler_16();
+
+
+/**
+ * @brief  Set ADC prescaler for 32.
+ * @return none
+ */
+void ACD_set_prescaler_32();
+
+
+/**
+ * @brief  Set ADC prescaler for 64
+ * @return none
+ */
+void ACD_set_prescaler_64();
+
+
+/**
+ * @brief  Set ADC prescaler for 128.
+ * @return none
+ */
+void ACD_set_prescaler_128();
+
+
+/**
+ * @brief  Start ADC conversion.
+ * @return none
+ */
+void ACD_start_conv();
+
+/** @} */
+
+#endif
