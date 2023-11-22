a7435ba918b18cefbe4ee689e32213b75217dfe7
Slavek Rylich <xrylic00@vut.cz>
2023-11-22 14:37:48 +0100

    update project

--- project/PROJECT-Measuring-electricity/src/main.c
+++ project/PROJECT-Measuring-electricity/src/main.c
@@ -33,16 +33,24 @@
 /* Global variables --------------------------------------------------*/
 /* Global variables --------------------------------------------------*/
 // Declaration of "dht12" variable with structure "DHT_values_structure"
-struct DHT_values_structure {
+
+/* struct DHT_values_structure {
    uint8_t hum_int;
    uint8_t hum_dec;
    uint8_t temp_int;
    uint8_t temp_dec;
    uint8_t checksum;
-} dht12;
+} dht12; */
+
+struct Measure_data {
+   uint8_t voltage;
+   uint8_t current;
+   uint8_t capacitance;
+   uint8_t resistance;
+} m_data;
 
 // Flag for printing new data from sensor
-volatile uint8_t new_sensor_data = 0;
+//volatile uint8_t new_sensor_data = 0;
 
 
 // Slave and internal addresses of temperature/humidity sensor DHT12
@@ -66,7 +74,7 @@ int main(void)
     twi_init();
 
     // UART
-    uart_init(UART_BAUD_SELECT(115200, F_CPU));
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
 
 
     //OLED
@@ -74,22 +82,32 @@ int main(void)
     oled_clrscr();
 
     oled_charMode(DOUBLESIZE);
-    oled_puts("OLED disp.");
+    //oled_puts("OLED disp.");
+    oled_puts("MULTIMETR");
 
     oled_charMode(NORMALSIZE);
 
     // oled_gotoxy(x, y)
     oled_gotoxy(0, 2);
-    oled_puts("128x64, SHH1106");
+    //oled_puts("128x64, SHH1106");
+    oled_puts("DE2 - projekt 2023");
 
-    oled_gotoxy(0, 4);
-    oled_puts("BPC-DE2, Brno");
+    oled_gotoxy(0, 3);
+    //oled_puts("BPC-DE2, Brno");
+    oled_puts("Voltage:");
 
     // oled_drawLine(x1, y1, x2, y2, color)
     oled_drawLine(0, 25, 120, 25, WHITE);
 
+    oled_gotoxy(0, 4);
+    //oled_puts("Hum     Temp");
+    oled_puts("Current:");
+
     oled_gotoxy(0, 5);
-    oled_puts("Hum     Temp");
+    oled_puts("Capacitance:");
+
+    oled_gotoxy(0, 6);
+    oled_puts("Resistance:");
 
     
 
@@ -118,12 +136,16 @@ int main(void)
 
     // Enable ADC module
     ADCSRA |= (1<<ADEN);
+    ACD_enable();
+
 
     // Enable conversion complete interrupt
     ADCSRA |= (1<<ADIE);
+    ACD_enable_interrupt()
 
     // Set clock prescaler to 128
-    ADCSRA |= ((1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0)); 
+    ADCSRA |= ((1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0));
+    ACD_set_prescaler_128(); 
 
     // Configuration of 8-bit Timer/Counter0 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
@@ -143,6 +165,7 @@ int main(void)
     // Enables interrupts by setting the global interrupt mask
     sei();  
 
+    oled_display();
 
     // Infinite loop
     while (1) {
@@ -201,7 +224,7 @@ int main(void)
 * Function: Timer/Counter1 overflow interrupt
 * Purpose:  Read temperature and humidity from DHT12, SLA = 0x5c.
 **********************************************************************/
-ISR(TIMER1_OVF_vect)
+/* ISR(TIMER1_OVF_vect)
 {
     // Test ACK from sensor
     twi_start();
@@ -223,14 +246,89 @@ ISR(TIMER1_OVF_vect)
         new_sensor_data = 1;
     }
     twi_stop();
+} */
+
+ISR(TIMER1_OVF_vect)
+{
+  static uint8_t no_of_overflows = 0;
+
+  no_of_overflows++;
+  
+  if(no_of_overflows >=3) {
+    // Start ADC conversion
+    ACD_start_conv();
+  }
+
 }
 
-ISR (TIMER1_OVF_vect)
+ISR(ADC_vect)
 {
-    static uint8_t no_of_overflow = 0;
+    static uint8_t channel = 0; //statemachine
+    uint16_t value;
+    char string[4];  // String for converted numbers by itoa()
 
-    no_of_overflow++;
-    if (no_of_overflow >= 3){
-        
+    // Read converted value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
+    value = ADC;
+    // Convert "value" to "string" and display it
+    // itoa(value,string,10);
+    // lcd_gotoxy(1, 0); lcd_puts("value:");
+    // lcd_gotoxy(3, 1); lcd_puts("key:");
+    // lcd_gotoxy(8, 0); lcd_puts(string);  // Put ADC value in decimal
+
+    // itoa(value,string,16);
+    // lcd_gotoxy(13,0); lcd_puts(string);  // Put ADC value in hexadecimal
+    // lcd_gotoxy(8, 1); lcd_putc(value);  // Put button name here
+
+    switch (channel)
+    {
+    case 0:
+        m_data.voltage = value;
+
+        itoa(m_data.voltage, string, 10);
+        uart_puts(string);
+        oled_gotoxy(14, 3);
+        oled_puts(string);
+        //oled_puts(".");
+        //uart_puts(".");
+
+        channel = 1;
+        ACD_select_channel_A1();
+        break;
+    case 1:
+        m_data.current = value;
+
+        itoa(m_data.voltage, string, 10);
+        uart_puts(string);
+        oled_gotoxy(14, 4);
+        oled_puts(string);
+
+        channel = 2;
+        ACD_select_channel_A2();
+        break;
+    case 2:
+        m_data.capacitance = value;
+
+        itoa(m_data.voltage, string, 10);
+        uart_puts(string);
+        oled_gotoxy(14, 5);
+        oled_puts(string);
+
+        channel = 3;
+        ACD_select_channel_A3();
+        break;
+    
+    default:
+        m_data.resistance = value;
+
+        itoa(m_data.voltage, string, 10);
+        uart_puts(string);
+        oled_gotoxy(14, 6);
+        oled_puts(string);
+
+        channel = 0;
+        ACD_select_channel_A0();
+        break;
     }
+
 }
